4c92102f511bfb0736a932d6d5624b6cf194a242
Tim Martin <timothy.m.martin@gmail.com>
2015-02-04 16:50:23 -0800

    (#11) Update karma tests for messages.

--- client/components/message/message.service.spec.js
+++ client/components/message/message.service.spec.js
@@ -12,36 +12,30 @@ describe('MyController', function() {
   // load controller views
   var views = ['app/main/main.html'];
 
-  views.forEach(function (view) {
+  views.forEach(function(view) {
     beforeEach(module(view));
   });
 
-  beforeEach(inject(function (_messageService_, _$httpBackend_, _Auth_) {
-
+  beforeEach(inject(function(_messageService_, _$httpBackend_, _Auth_) {
     Auth = _Auth_;
     messageService = _messageService_;
     $httpBackend = _$httpBackend_;
-
   }));
 
-  describe('inbox', function () {
-
-    it('fetches a list of private messages', function () {
-      $httpBackend.expectGET('/api/messages/inbox').respond(function (method, url, data) {
+  describe('inbox', function() {
+    it('fetches a list of private messages', function() {
+      $httpBackend.expectGET('/api/messages/inbox').respond(function(method, url, data) {
         return [200, [{}, {}, {}, {}]];
       });
-      messageService.inbox().then(function (messages) {
+      messageService.getInbox().then(function(messages) {
         expect(messages.length).toBe(4);
       });
-
       $httpBackend.flush();
     });
-
   });
 
-  describe('update', function () {
-
-    it('updates the read property', function () {
+  describe('update', function() {
+    it('updates the read property', function() {
       var message = {
         to: '123',
         from: '123',
@@ -49,22 +43,21 @@ describe('MyController', function() {
         content: 'Content 1',
         read: false
       };
-      $httpBackend.expectPUT('/api/messages/update').respond(function (method, url, data) {
+      $httpBackend.expectPUT('/api/messages/update').respond(function(method, url, data) {
         message.read = true;
         return [200, message];
       });
-      messageService.update(message, {read: true}).then(function (message) {
+      messageService.update(message, {
+        read: true
+      }).then(function(message) {
         expect(message.read).toBe(true);
       });
-
       $httpBackend.flush();
     });
-
   });
 
-  describe('create', function () {
-
-    it('creates a new message', function () {
+  describe('create', function() {
+    it('creates a new message', function() {
       var message = {
         to: '123',
         from: '123',
@@ -72,18 +65,18 @@ describe('MyController', function() {
         message: 'Content 1',
         read: false
       };
-      spyOn(Auth, 'getCurrentUser').andCallFake(function () {
-        return { github: { id: '123' } };
+      spyOn(Auth, 'getCurrentUser').andCallFake(function() {
+        return {
+          github: {
+            id: '123'
+          }
+        };
       });
       $httpBackend.expectPOST('/api/messages/create').respond(200, true);
-      messageService.create(message).then(function (result) {
+      messageService.create(message).then(function(result) {
         expect(result).toBe(true);
       });
-
       $httpBackend.flush();
-
     });
-
   });
-
 });
